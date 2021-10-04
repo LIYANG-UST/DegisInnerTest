ff539f2b0c263fcbeec7178d129f66ccf3416983
LIYang <liyangTJU@outlook.com>
2021-10-04 21:44:48 +0800

    load some contract

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - Bootstrap v4.6.0
 - jQuery v3.5.1
 - web3 v1.6.0
+- truffle/contract v4.3.37
+- evm-chains v0.2.0
 
 ## File Structure
 
--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const evmChains = window.evmChains;
 let web3Modal;
 let provider;
 let selectedAccount;
+let contracts = {};
+let contractAddress = {};
 
 function init() {
   console.log("Initializing example");
@@ -58,10 +60,169 @@ function init() {
   console.log("Web3Modal instance is", web3Modal);
 }
 
+async function initContract() {
+  $.getJSON("../abis/DegisToken.json", function (data) {
+    contracts.DegisToken = TruffleContract(data);
+    contracts.DegisToken.setProvider(provider);
+  });
+  $.getJSON("../abis/InsurancePool.json", function (data) {
+    contracts.InsurancePool = TruffleContract(data);
+    contracts.InsurancePool.setProvider(provider);
+  });
+  $.getJSON("../abis/PolicyFlow.json", function (data) {
+    contracts.PolicyFlow = TruffleContract(data);
+    contracts.PolicyFlow.setProvider(provider);
+  });
+  $.getJSON("../abis/PolicyToken.json", function (data) {
+    contracts.PolicyToken = TruffleContract(data);
+    contracts.PolicyToken.setProvider(provider);
+  });
+}
+
+async function initContractAddress() {
+  $.getJSON("../address.json", function (data) {
+    contractAddress.DegisToken = data.DegisToken;
+    console.log("address ss", data.DegisToken);
+  });
+}
+
+function bindEvents() {
+  $(document).on("click", ".btn-passMinter", passMinter);
+}
+
+async function fetchAccountData() {
+  // Get a Web3 instance for the wallet
+  const web3 = new Web3(provider);
+
+  console.log("Web3 instance is", web3);
+
+  // Get connected chain id from Ethereum node
+  const chainId = await web3.eth.getChainId();
+  // Load chain information over an HTTP API
+  const chainData = evmChains.getChain(chainId);
+  document.querySelector("#network-name").textContent = chainData.name;
+
+  // Get list of accounts of the connected wallet
+  const accounts = await web3.eth.getAccounts();
+
+  // MetaMask does not give you all accounts, only the selected account
+  console.log("Got accounts", accounts);
+  selectedAccount = accounts[0];
+
+  document.querySelector("#selected-account").textContent = selectedAccount;
+
+  // Get a handl
+  const template = document.querySelector("#template-balance");
+  const accountContainer = document.querySelector("#accounts");
+
+  // Purge UI elements any previously loaded accounts
+  accountContainer.innerHTML = "";
+
+  // Go through all accounts and get their ETH balance
+  const rowResolvers = accounts.map(async (address) => {
+    const balance = await web3.eth.getBalance(address);
+    // ethBalance is a BigNumber instance
+    // https://github.com/indutny/bn.js/
+    const ethBalance = web3.utils.fromWei(balance, "ether");
+    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
+    // Fill in the templated row and put in the document
+    const clone = template.content.cloneNode(true);
+    clone.querySelector(".address").textContent = address;
+    clone.querySelector(".balance").textContent = humanFriendlyBalance;
+    accountContainer.appendChild(clone);
+  });
+
+  // Because rendering account does its own RPC commucation
+  // with Ethereum node, we do not want to display any results
+  // until data for all accounts is loaded
+  await Promise.all(rowResolvers);
+
+  // Display fully loaded UI for wallet data
+  document.querySelector("#prepare").style.display = "none";
+  document.querySelector("#connected").style.display = "block";
+}
+
+async function refreshAccountData() {
+  // If any current data is displayed when
+  // the user is switching acounts in the wallet
+  // immediate hide this data
+  document.querySelector("#connected").style.display = "none";
+  document.querySelector("#prepare").style.display = "block";
+
+  // Disable button while UI is loading.
+  // fetchAccountData() will take a while as it communicates
+  // with Ethereum node via JSON-RPC and loads chain data
+  // over an API call.
+  document.querySelector("#btn-connect").setAttribute("disabled", "disabled");
+  await fetchAccountData(provider);
+  document.querySelector("#btn-connect").removeAttribute("disabled");
+}
+
+async function onConnect() {
+  console.log("Opening a dialog", web3Modal);
+  try {
+    provider = await web3Modal.connect();
+  } catch (e) {
+    console.log("Could not get a wallet connection", e);
+    return;
+  }
+
+  // Subscribe to accounts change
+  provider.on("accountsChanged", (accounts) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to chainId change
+  provider.on("chainChanged", (chainId) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to networkId change
+  provider.on("networkChanged", (networkId) => {
+    fetchAccountData();
+  });
+
+  document.querySelector("#nomessage").style.display = "none";
+
+  await refreshAccountData();
+  await initContract();
+  await initContractAddress();
+}
+
+async function onDisconnect() {
+  console.log("Killing the wallet connection", provider);
+
+  // TODO: Which providers have close method?
+  if (provider.close) {
+    await provider.close();
+
+    // If the cached provider is not cleared,
+    // WalletConnect will default to the existing session
+    // and does not allow to re-scan the QR code with a new wallet.
+    // Depending on your use case you may want or want not his behavir.
+    await web3Modal.clearCachedProvider();
+    provider = null;
+  }
+
+  selectedAccount = null;
+
+  // Set the UI back to the initial state
+  document.querySelector("#prepare").style.display = "block";
+  document.querySelector("#connected").style.display = "none";
+  document.querySelector("#nomessage").style.display = "block";
+}
+
+async function passMinter() {
+  const degis = await contracts.DegisToken.at(contractAddress.DegisToken);
+  const balance = await degis.balanceOf(selectedAccount);
+  console.log("user balance:", parseInt(balance) / 10 ** 18);
+}
+
 window.addEventListener("load", async () => {
   init();
   document.querySelector("#btn-connect").addEventListener("click", onConnect);
   document
     .querySelector("#btn-disconnect")
     .addEventListener("click", onDisconnect);
+  bindEvents();
 });
